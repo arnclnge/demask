713a8cb425862b043cc81564e0f7c0db41b6b7e4
Johannes Nowe <johannes.nowe@vliz.be>
2026-01-12 11:39:40 +0100

    adjust ICES_datras_analysis.qmd to allow invertebrates and run analysis

--- scripts/parametrized_analysis.R
+++ scripts/parametrized_analysis.R
@@ -1,10 +1,13 @@
 library(quarto)
 
+
+# FISH --------------------------------------------------------------------
 quarto_render(
   input = file.path("scripts",
                     "ICES_datras_analysis.qmd"),
   output_file = file.path("results_cod.html"),
   execute_params = list(species = "Gadus morhua",
+                        group = "fish", #because code also works for invertebrates now
                         plot_limit = 100,#Change this to change the maximum value plotted,
 #Everything above this value gets the same value and color, for plot clarity reasons
 years_mask = 0.90,
@@ -16,6 +19,7 @@ quarto_render(
                     "ICES_datras_analysis.qmd"),
   output_file = file.path("results_haddock.html"),
   execute_params = list(species = "Melanogrammus aeglefinus",
+                        group = "fish",
                         plot_limit = 100,
                         years_mask = 0.90,
                         consistent_thr = 0.7))
@@ -25,6 +29,7 @@ quarto_render(
                     "ICES_datras_analysis.qmd"),
   output_file = file.path("results_seabass.html"),
   execute_params = list(species = "Dicentrarchus labrax",
+                        group = "fish",
                         plot_limit = 100,
                         years_mask = 0.70,
                         consistent_thr =0.6)) #otherwise no grid cells in summer, summer2015&2016 no data
@@ -34,6 +39,7 @@ quarto_render(
                     "ICES_datras_analysis.qmd"),
   output_file = file.path("results_ling.html"),
   execute_params = list(species = "Molva molva",
+                        group = "fish",
                         plot_limit = 100,
                         years_mask = 0.90,
                         consistent_thr = 0.6))
@@ -43,6 +49,56 @@ quarto_render(
                     "ICES_datras_analysis.qmd"),
   output_file = file.path("results_herring.html"),
   execute_params = list(species = "Clupea harengus",
+                        group = "fish",
                         plot_limit = 100,
                         years_mask = 0.90,
                         consistent_thr = 0.6))
+
+
+# INVERTEBRATES -----------------------------------------------------------
+quarto_render(
+  input = file.path("scripts",
+                    "ICES_datras_analysis.qmd"),
+  output_file = file.path("results_europeanlobster.html"),
+  execute_params = list(species = "Homarus gammarus",
+                        group = "invertebrates",
+                        plot_limit = 100,
+                        years_mask = 0.70,
+                        consistent_thr = 0.6))
+quarto_render(
+  input = file.path("scripts",
+                    "ICES_datras_analysis.qmd"),
+  output_file = file.path("results_cuttlefish.html"),
+  execute_params = list(species = "Sepia officinalis",
+                        group = "invertebrates",
+                        plot_limit = 100,
+                        years_mask = 0.1, #not a lot of years with data
+                        consistent_thr = 0.6))
+quarto_render(
+  input = file.path("scripts",
+                    "ICES_datras_analysis.qmd"),
+  output_file = file.path("results_scallop.html"),
+  execute_params = list(species = "Pecten maximus",
+                        group = "invertebrates",
+                        plot_limit = 100,
+                        years_mask = 0.10,
+                        consistent_thr = 0.6))
+quarto_render(
+  input = file.path("scripts",
+                    "ICES_datras_analysis.qmd"),
+  output_file = file.path("results_norwaylobster.html"),
+  execute_params = list(species = "Nephrops norvegicus",
+                        group = "invertebrates",
+                        plot_limit = 100,
+                        years_mask = 0.10,
+                        consistent_thr = 0.6))
+quarto_render(
+  input = file.path("scripts",
+                    "ICES_datras_analysis.qmd"),
+  output_file = file.path("results_mussel.html"),
+  execute_params = list(species = "Mytilus edulis",
+                        group = "invertebrates",
+                        plot_limit = 100,
+                        years_mask = 0.05,
+                        consistent_thr = 0.6))
+
